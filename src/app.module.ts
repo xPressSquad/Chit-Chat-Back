05833dff871c6eafa8f1f12410ab90e43771ddc2
youness <younesserbai@gmail.com>
2024-11-15 12:08:45 +0100

    CC-8: Configure server module and integrate with app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ServerModule } from './server/server.module';
+  import { Module } from '@nestjs/common';
+  import { AppController } from './app.controller';
+  import { AppService } from './app.service';
+  import { ConfigModule } from '@nestjs/config';
+  import { MongooseModule } from '@nestjs/mongoose';
+  import { ServerModule } from './server/server.module';
+  import { ServeStaticModule } from '@nestjs/serve-static';
+  import { join } from 'path';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    ServerModule, // This was missing a closing parenthesis
-    MongooseModule.forRoot(process.env.DB_URI),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+  @Module({
+    imports: [
+      ConfigModule.forRoot({
+        envFilePath: '.env',
+        isGlobal: true,
+      }),
+      ServerModule, 
+      MongooseModule.forRoot(process.env.DB_URI),
+      ServeStaticModule.forRoot({
+        rootPath: join(__dirname, '..', 'uploads'),
+        serveRoot: '/uploads',
+      }),
+
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+  })
+  export class AppModule {}
+
+
--- src/server/server.module.ts
+++ src/server/server.module.ts
@@ -1,13 +1,20 @@
-// src/server/server.module.ts
+
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Server, ServerSchema } from './server.schema';
-import { ServerService } from './server.service';
 import { ServerController } from './server.controller';
+import { ServerService } from './server.service';
+import { Server, ServerSchema } from './server.schema';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Server.name, schema: ServerSchema }])],
-  providers: [ServerService],
+  imports: [
+    MongooseModule.forFeature([{ name: Server.name, schema: ServerSchema }]),
+    MulterModule.register({
+      dest: './uploads/servers',
+    }),
+  ],
   controllers: [ServerController],
+  providers: [ServerService],
+  exports: [ServerService],
 })
 export class ServerModule {}
